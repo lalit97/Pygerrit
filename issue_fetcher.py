a801bd1624113885b9b56f5031898ac75a77fc39
lalit97 <sutharlalit.97@gmail.com>
2019-03-05 00:56:15 +0530

    added exception handling

--- issue_fetcher.py
+++ issue_fetcher.py
@@ -1,9 +1,53 @@
 import requests 
 import json 
+import sys 
+from datetime import datetime
+from collections import Counter 
+import logging
 
 class StatusType:
-	MERGED = 'merged'
-	OPEN = 'open'
+	MERGED = 'MERGED'
+	OPEN = 'OPEN'
+
+
+def logger(error_code, error_message):
+	'''
+	this handles error messages
+	'''
+	logging.error(error_message)
+
+
+def clean_date(date_string):
+	'''
+	this cleans the date 
+	'''
+	try:
+		datetime.strptime(date_string,'%Y-%m-%d')
+	except ValueError as e:
+		error_message = "please enter date in yyyy-mm-dd"
+		logger(e, error_message)
+		sys.exit()
+
+
+def clean_input(owner_name, status, start_date=None, end_date=None):
+	'''
+	This cleans the user input and creates query parameters 
+	'''
+	query_params = {
+		'status': status,
+		'owner': owner_name
+	}
+
+	if start_date:
+		clean_date(start_date)
+		query_params['after'] = start_date
+
+	if end_date:
+		clean_date(end_date)
+		query_params['before'] = end_date
+
+	return query_params
+
 
 def format_query_params(query_params):
 	'''
@@ -18,36 +62,57 @@ def format_query_params(query_params):
 		formatted_query = formatted_query[1:]
 	return formatted_query
 
-def get_changes(owner_name, status, start_date=None, end_date=None):
-	'''
-	This returns json data based on any query 
-	'''
-	url = 'http://gerrit.wikimedia.org/r/changes/?q='
-	query_params = {
-		'status': status,
-		'owner': owner_name
-	}
-	if start_date:
-		query_params['after'] = start_date
-	if end_date:
-		query_params['before'] = end_date
-	query = format_query_params(query_params)
-	url = url + query
 
-	response = requests.get(url)
+def fetech_gerrit_data(url, formatted_query):
+	'''
+	this gets json response from given queries 
+	'''
+	url += formatted_query
+
+	try:
+		response = requests.get(url)
+	except requests.exceptions.RequestException as e:
+		error_message = "please enter a valid url"
+		logger(e, error_message)
+		sys.exit()
+
 	response_text = response.text
 	response_text = response_text.replace(")]}'","",1)
-	json_data = json.loads(response_text)
+
+	try:
+		json_data = json.loads(response_text)
+	except json.decoder.JSONDecodeError as e:
+		error_message = "please enter a valid username"
+		logger(e, error_message)
+		sys.exit()
 
 	return json_data
 
+
+def get_count(json_data, status_type):
+	'''
+	this returns count of given status type in json 
+	'''
+	count_dict = Counter(data['status'] for data in json_data)
+	return count_dict[status_type]
+
 if __name__ == '__main__':
 	owner_name = input("enter username > ")
-	print ("enter date in yyyy-mm-dd format, eg: 2018-01-15")
-	start_date = input("enter starting date > ")
-	end_date = input("enter ending date > ")
-	status = StatusType.MERGED
+	start_date, end_date = None, None
 
-	changes = get_changes(owner_name, status, start_date, end_date)
-	merge_count = len(changes)
-	print ('Number of patches merged : {}'.format(merge_count))
+	timeframe = input("press 'y' to search within a timeframe > ")
+	if timeframe == 'y' or timeframe == 'Y':
+		print ("enter date in yyyy-mm-dd format, eg: 2018-01-15")
+		start_date = input("enter starting date > ")
+		end_date = input("enter ending date > ")
+
+	status_type = StatusType.MERGED
+	url = 'http://gerrit.wikimedia.org/r/changes/?q=' 
+
+	query_params = clean_input(owner_name, status_type, start_date, end_date)
+	formatted_query = format_query_params(query_params)
+	json_data = fetech_gerrit_data(url, formatted_query)
+	count = get_count(json_data, status_type)
+
+	print ('Number of patches {} : {}'.format(status_type.lower(),count))
+
